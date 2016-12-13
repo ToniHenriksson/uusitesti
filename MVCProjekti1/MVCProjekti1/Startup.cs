ba69ebffd39861de770ce2d0800cf5a4304837ae
Toni Henriksson <Toni Henriksson@DESKTOP-39ESCD8>
2016-12-13 19:02:43 +0200

    Harjoitus projekti aloitettu. Ei koodattu vielä mitään.

---
+++ MVCProjekti1/MVCProjekti1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProjekti1.Startup))]
+namespace MVCProjekti1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
